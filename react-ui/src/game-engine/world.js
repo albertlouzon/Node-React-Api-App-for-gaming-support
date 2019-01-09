6a080e7188f7467fc3263aebfd2245268a3b509b
Albert <albertlouzon@gmail.com>
2019-01-09 22:53:41 +0200

    nique la chatte a ta mere et ta grand mere

--- react-ui/src/game-engine/world.js
+++ react-ui/src/game-engine/world.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react'
 
-const worldHeight = 20000
 const topDelta = 400
 const screeHeight= window.screen.height
 const speed = 1
 const playerSize = {height:40,width:40}
-
 export default class World extends Component {
   constructor(props) {
     super(props)
     
     this.state = {
       isGameStarted:false,
-      top:worldHeight,
-      blockTop:19500,
+      bottom:-50,
       deltaTop:50,
+       worldHeight : 300,
+       blockTop:300,
+
     }
     this.w = true
   }
   
   componentDidMount() {
-    this.ballMagnetic.scrollIntoView()
-    this.interval = setInterval(() => {this.setState({top:this.state.top -speed});
-    this.ballMagnetic.scrollIntoView()  
-    if(this.state.top-this.state.deltaTop-playerSize.height === this.state.blockTop){
+    this.ball.scrollIntoView()
+    this.interval = setInterval(() => {this.setState({bottom:this.state.bottom - speed,
+      worldHeight:this.state.worldHeight+speed
+    });
+    this.ball.scrollIntoView()  
+    if(this.state.bottom+this.state.deltaTop-playerSize.height === this.state.blockTop){
       alert('ok')
     }
   }, 1);
@@ -44,41 +46,36 @@ export default class World extends Component {
 
     const   styles = {
       bigBox :{
-          height : worldHeight,
-          width : '50vw',
-          backgroundColor:'red'
-      },
-    
-      ballMagnetic:{
-        position:'absolute',
-        height:screeHeight/2,
-        top:this.state.top-screeHeight/2
+          height : this.state.worldHeight,
+          width : '100vw',
+          backgroundColor:'red',
+          position:'absolute',
 
       },
+    
+     
       ball:{
         left:400,
         height:playerSize.height,
         width:playerSize.width,
         borderRadius:'50%',
         backgroundColor:'yellow',
-        position:'absolute',
-        bottom:0
+        position:'relative',
+        bottom:this.state.bottom
       },
       block:{
-        position:'absolute',
+        position:'relative',
         height:this.state.deltaTop,
         width:50,
         backgroundColor:'green',  
         left:400,
-        top:this.state.blockTop
+        bottom:this.state.worldHeight-300
       }
     }
     return (
       <div style={styles.bigBox} >
       <div style={styles.block}></div>
-        <div style={styles.ballMagnetic} ref={(el) => { this.ballMagnetic = el; }}>
         <div style={styles.ball} ref={(el) => { this.ball = el; }} > </div>
-        </div>
       </div>
     )
 
