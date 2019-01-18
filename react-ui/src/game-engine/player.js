cdf565190115fb5418916eabc0a6b33f007d6b99
johanelbg <johan.elberg38@gmail.com>
2019-01-18 20:44:37 +0200

    added x axis

--- react-ui/src/game-engine/player.js
+++ react-ui/src/game-engine/player.js
@@ -1,51 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 export const playerSize = { height: 40, width: 40 };
 
 export default class Player extends Component {
-constructor(props) {
-  super(props)
-    this.bottom = this.props.bottom
-  this.state = {
-     
+  constructor(props) {
+    super(props);
+    this.bottom = this.props.bottom;
+    this.state = {
+      xPosition: 50
+    };
   }
-}
 
-componentWillUpdate(nextProps, nextState) {
-    if (
-        nextProps.bottom ===
-      this.props.bottom
-    ) {
-
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.bottom === this.props.bottom) {
     } else {
-      console.log('ya du mouvement')
-      this.player.scrollIntoView()
+      console.log("ya du mouvement");
+      this.player.scrollIntoView();
     }
+
+    document.addEventListener("keydown", this.handleXposition);
   }
 
-    render() {
-     
-        const styles = {
-            ball: {
-              left: 400,
-              height: playerSize.height,
-              width: playerSize.width,
-              borderRadius: "50%",
-              backgroundColor: "yellow",
-              position: "relative",
-              bottom: this.props.bottom
-            },
-      
-          };
+  handleXposition = e => {
+    // if (this.state.xPosition === 100) return;
+
+    switch (e.key) {
+      case "q":
+        this.setState({
+          xPosition: this.state.xPosition + 1
+        });
+        break;
+      case "d":
+      case "q":
+        this.setState({
+          xPosition: this.state.xPosition - 1
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  render() {
+    const styles = {
+      ball: {
+        left: `calc(100% - ${this.state.xPosition}%)`,
+        height: playerSize.height,
+        width: playerSize.width,
+        borderRadius: "50%",
+        backgroundColor: "yellow",
+        position: "relative",
+        bottom: this.props.bottom
+      }
+    };
     return (
-      <div>
-        
-        <div
-          style={styles.ball}
-          ref={el => {
-            this.player = el;
-          }}
-        ></div>
-      </div>
-    )
+      <div
+        style={styles.ball}
+        ref={el => {
+          this.player = el;
+        }}
+      />
+    );
   }
 }
